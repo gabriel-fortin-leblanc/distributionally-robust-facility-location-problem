a057bc8ab9132410842333159130be14c14494cb
Gabriel Fortin-Leblanc <gabrielfortinleblanc@gmail.com>
2024-11-14 17:20:53 -0500

    Add others tests for the FLP class

--- tests/ddro/test_pg.py
+++ tests/ddro/test_pg.py
@@ -16,12 +16,19 @@ class TestFLP:
                 np.array([10, 20]),
                 np.array([[10, 20], [20, 30]]),
                 np.array([1, 2]),
-            )
+            ),
+            (
+                2,
+                3,
+                np.array([1, 2, 3]),
+                np.array([1, 2]),
+                np.array([[1, 2, 3], [3, 4, 5]]),
+                np.array([1, 2]),
+            ),
         ],
     )
     def test_init(self, nf, nc, sd, oc, tc, cf):
         """No error in init"""
-        print(cf.shape)
         flp = pg.FLP(nf, nc, sd, oc, tc, cf)
         assert flp.nf == nf
         assert flp.nc == nc
